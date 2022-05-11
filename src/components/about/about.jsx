f251e872c1efc178671be0ff97b7ee8f6696ea23
jamesgraham0 <jamesgraham0@users.noreply.github.com>
2022-05-11 01:40:54 -0500

    Fixed bugs

---
+++ src/components/about/about.jsx
@@ -0,0 +1,12 @@
+import "./about.css";
+import Projects from "../projects/projects";
+
+function About() {
+    return (
+        <div id="projects" className="container about-container">
+            <Projects />
+        </div>
+    )
+}
+
+export default About;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,17 @@
+import './header.css';
+
+function Header() {
+    return (
+        <div id='home' className='container header-container'>
+            <h3>
+                <p>
+                    I<span>'</span>m
+                </p>
+                James
+            </h3>
+            <h2>Computer Science is my passion</h2>
+        </div>
+    );
+}
+
+export default Header;
