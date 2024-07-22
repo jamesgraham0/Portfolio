d3a29ecbffe35bdec9b1f8083e8a51fd6f60b8ce
James Graham <jgraham.paul@gmail.com>
2024-07-22 16:26:44 -0500

    Remove scroll-down when window size < 820px, removed horizontal scrolling

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -34,7 +34,7 @@ function Home() {
         </div>
         <div className='click-me-container' hidden={isActive}>
           <p className={`click-me ${isActive ? 'active' : ''}`}>Click me</p>
-          <svg id="svg" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+          <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
             <path strokeLinecap="round" strokeLinejoin="round" className={`draw-arrow ${isActive ? 'active' : ''}`} d="M11.3,2.5c-5.8,5-8.7,12.7-9,20.3s2,15.1,5.3,22c6.7,14,18,25.8,31.7,33.1" />
             <path strokeLinecap="round" strokeLinejoin="round" className={`tail-1 ${isActive ? 'active' : ''}`} d="M40.6,78.1C39,71.3,37.2,64.6,35.2,58" />
             <path strokeLinecap="round" strokeLinejoin="round" className={`tail-2 ${isActive ? 'active' : ''}`} d="M39.8,78.5c-7.2,1.7-14.3,3.3-21.5,4.9" />
